c9c6e09e4b2b793044aa1b018aafb2eccccaf9f4
mourad5000 <mouradmounim7@gmail.com>
2021-08-13 14:27:23 +0200

    Add the cart icon to wrapper

--- src/components/AppWrapper/AppWrapper.jsx
+++ src/components/AppWrapper/AppWrapper.jsx
@@ -1,14 +1,33 @@
+/* eslint-disable jsx-a11y/interactive-supports-focus */
 /* eslint-disable react/prop-types */
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import "./AppWrapper.scss";
 
+import shoppingCart from "../../assets/images/shopping-cart.svg";
+
 function AppWrapper({ children }) {
+  const history = useHistory();
+  const { cartItems } = useSelector(({ cartReducer }) => cartReducer);
+
+  function handleClickCart() {
+    history.push("/myCart");
+  }
+
   return (
     <div className="app-wrapper-container">
       <div className="app-wrapper">
         {children}
       </div>
+      {
+        cartItems.length > 0 && (
+        <div className="app-wrapper__shoppin-cart" onClick={handleClickCart} role="button">
+          <img className="shoppin-cart__img" src={shoppingCart} alt="Shopping cart" />
+        </div>
+        )
+      }
     </div>
   );
 }
